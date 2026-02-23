e031844ef4997938ecace4a09a2a0f6b01c2fc9b
Santiago <santitareas19@gmail.com>
2026-02-23 11:00:58 +0000

    feat: mejorar formato de mensajes en consola

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello World - SDFEngine Learning!");
+console.log("Proyecto iniciado correctamente");
+console.log("Fecha:", new Date().toISOString());
+console.log("Aprendiendo Git y GitHub!");
+console.log("Rama actual: feature/mejoras-mensaje");
+console.log("=".repeat(50));
